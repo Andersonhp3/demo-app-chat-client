89153b4a61cd9f3db48afac2ee1b74535966fa08
Andersonhp3 <arhp1591@gmail.com>
2022-09-04 12:21:27 -0300

    feat: demo app chat client with connection mongodb

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgev7dl.mongodb.net/?retryWrites=true&w=majority`
+
+let Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
+})
+
+app.post('/messages', (req, res) => {
+  let message = new Message(req.body)
+
+  message.save((err) => {
+    if(err) {
+      sendStatus(500)
+    }
+  })
+
+  io.emit('message', req.body)
+  res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+  console.log('use connection');
+})
+
+mongoose.connect(dbUrl, (err) => {
+  console.log('mongo db connection', err);
+})
+
+const server = http.listen(3000, () => {
+  console.log('server is listening on port', server.address().port);
+})
